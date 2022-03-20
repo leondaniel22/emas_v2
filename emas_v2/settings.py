5680a3752e2dbac5a0921af0a941deccc1ec3001
Leon <leon.klaus@fau.de>
2022-03-20 18:09:53 +0100

    'Initial commit

--- emas_v2/settings.py
+++ emas_v2/settings.py
@@ -59,8 +59,9 @@ INSTALLED_APPS = [
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
