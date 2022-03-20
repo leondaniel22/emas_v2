2439c478eae60985a1a665f04c5c3896ebfcc76d
Leon <leon.klaus@fau.de>
2022-03-20 17:56:55 +0100

    'Initial commit

--- emas_v2/settings.py
+++ emas_v2/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os
 import posixpath
 import sys
+
+import django_heroku
 from django.utils.translation import ugettext_lazy as _
 import dj_database_url
 
@@ -171,4 +173,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
-
+django_heroku.settings(locals())
